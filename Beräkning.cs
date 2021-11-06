14e42409b7d307c2f323d5ea05d683ebd622846a
Tobias Lindberg <tobias.lindberg@ga.ntig.se>
2021-11-06 23:46:09 +0100

    Add project files.

---
+++ Beräkning.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExempel
+{
+    interface ISumma
+    {
+        double Addera();
+    }
+
+    class Summa : ISumma
+    {
+        double a, b;
+        public Summa(double a, double b) { this.a = a; this.b = b; }
+        public double Addera() { return a+b; }
+    }
+
+
+
+
+
+
+    interface IDifferens
+    {
+        double Subtrahera();
+    }
+    interface IProdukt
+    {
+        double Multiplicera();
+    }
+    interface IKvot
+    {
+        double Dividera();
+    }
+    class Beräkning : ISumma, IDifferens, IProdukt, IKvot
+    {
+        double a, b;
+        public Beräkning(double a, double b) { this.a = a; this.b = b; }
+        public double Addera(){return a + b;}
+        public double Subtrahera() { return a - b; }
+        public double Multiplicera() { return a * b; }
+        public double Dividera() { return a / b; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExempel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISumma summa = new Summa(3, 5);
+            double resultat = summa.Addera();
+
+            Console.WriteLine(resultat);
+
+
+
+            summa = new Beräkning(3, 5);
+
+
+            Console.ReadLine();
+        }
+    }
+}
